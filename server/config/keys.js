fdfb99d9a09c2691d6a7de5d9cbd3dc666a20e78
Mariya RYCHKOVA <mrychkov@e1r10p1.42.fr>
2018-02-08 13:07:22 +0100

    google auth done

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,7 @@
 module.exports = {
 	googleClientID: '693483342639-a3q3jq869cv3klk044pjhluri3df55m5.apps.googleusercontent.com',
 	googleClientSecret: 'zZlgpJCIcdEsuPUh9xICXPp3',
-	mongoURI: 'mongodb://Turandot:Jcnfgbr1!@ds123258.mlab.com:23258/so-luciole-dev'
+	mongoURI: 'mongodb://Turandot:Jcnfgbr1!@ds123258.mlab.com:23258/so-luciole-dev',
+	cookieKey: 'hvdfvuhsvpfnvcsdvbgfnfhnhngfbfdvfdvsfdbhgj'
 };
 
